b427bd703a708ba32d45798c7d069764effb7a77
Michitomo Nakahara <mi@michitomo.jp>
2015-08-01 08:25:51 +0000

    fixed insufficient check ex. 123-45678

--- lib/jpn_zipcode.rb
+++ lib/jpn_zipcode.rb
@@ -6,7 +6,7 @@ module JpnZipcode
 		def fmt_input(zipcode)
 			if zipcode.to_s =~ /^\d{7}$/
 				return zipcode
-			elsif zipcode.to_s =~ /^\d{3}-\d{4}/
+			elsif zipcode.to_s =~ /^\d{3}-\d{4}$/
 				zipcode = zipcode.delete("-")
 				return zipcode
 			else
