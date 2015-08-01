f9d0e046d4d3e10ffd44cab1cc542863d4b5fc7d
takeshitou <vagrant@enpit>
2015-08-01 10:01:57 +0000

    modify open csv

--- lib/jpn_zipcode.rb
+++ lib/jpn_zipcode.rb
@@ -1,13 +1,15 @@
+# -*- encoding: utf-8 -*-
 require "jpn_zipcode/version"
 
 module JpnZipcode
   class Main
    def open_csv(filename)
         zipHash = Hash.new
-        File.open(filename) do |csv|
+        File.open(filename) do |csvfile|
 	#File.open("resource/KEN_ALL_ROME.CSV") do |csvfile| 
 	   csvfile.each_line do |fileline|
 		zipHash[fileline.split(",")[0].to_s] = fileline
+		puts fileline
 	   end
 	end
 	return zipHash
