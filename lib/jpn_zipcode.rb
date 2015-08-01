3d744017944c5777fc5aed2394a32dc99591e9b8
Michitomo Nakahara <mi@michitomo.jp>
2015-08-01 07:26:27 +0000

    first commit for function fmt_input

--- lib/jpn_zipcode.rb
+++ lib/jpn_zipcode.rb
@@ -1,5 +1,16 @@
 require "jpn_zipcode/version"
 
 module JpnZipcode
-  # Your code goes here...
+
+	def fmt_input(zipcode)
+		if zipcode.to_s =~ /^\d{7}$/
+			return zipcode
+		elsif zipcode.to_s =~ /^\d{3}-\d{4}/
+			zipcode = zipcode.delete("-")
+			return zipcode
+		else
+			return ""
+		end
+	end
+
 end
