4aba87f9f4e9590d1edec554fccf21ae88190aef
Michitomo Nakahara <mi@michitomo.jp>
2015-08-01 07:33:03 +0000

    Added Main class

--- lib/jpn_zipcode.rb
+++ lib/jpn_zipcode.rb
@@ -1,16 +1,19 @@
 require "jpn_zipcode/version"
 
 module JpnZipcode
+	class Main
 
-	def fmt_input(zipcode)
-		if zipcode.to_s =~ /^\d{7}$/
-			return zipcode
-		elsif zipcode.to_s =~ /^\d{3}-\d{4}/
-			zipcode = zipcode.delete("-")
-			return zipcode
-		else
-			return ""
+		def fmt_input(zipcode)
+			if zipcode.to_s =~ /^\d{7}$/
+				return zipcode
+			elsif zipcode.to_s =~ /^\d{3}-\d{4}/
+				zipcode = zipcode.delete("-")
+				return zipcode
+			else
+				return ""
+			end
 		end
+
 	end
 
 end
