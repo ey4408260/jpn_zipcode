67c9bc13feb2a4d3d619b56d98545816385454bc
Etsuro Yoshino <a1549ey@aiitac.jp>
2015-08-01 08:36:12 +0000

    write discription

--- lib/jpn_zipcode.rb
+++ lib/jpn_zipcode.rb
@@ -1,5 +1,16 @@
 require "jpn_zipcode/version"
 
 module JpnZipcode
-  # Your code goes here...
+    def get_address(num)
+	filename = "http://xxx.csv"
+	phash = open_csv(filename)
+	postnum = fmt_input(num)
+	if phash.size = 0
+		return ""
+	end
+        if postnum.length = 0
+		return ""
+	end
+	return find_zip(postnum,phash)	
+    def
 end
