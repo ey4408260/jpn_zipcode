ab8361db4479a56ab0be0e2c32e480fad47429ad
Michitomo Nakahara <mi@michitomo.jp>
2015-08-01 10:44:28 +0000

    first release

--- lib/jpn_zipcode.rb
+++ lib/jpn_zipcode.rb
@@ -1,9 +1,40 @@
 # -*- encoding: utf-8 -*-
-require "jpn_zipcode/version"
-
 module JpnZipcode
   class Main
 
+
+    def fmt_input(zipcode)
+      if zipcode.to_s =~ /^\d{7}$/
+        return zipcode
+      elsif zipcode.to_s =~ /^\d{3}-\d{4}$/
+        zipcode = zipcode.delete("-")
+        return zipcode
+      else
+        return ""
+      end
+    end
+
+    def find_zip(zipcode, zip_hash)
+      address = zip_hash[zipcode]
+      if address.is_a? String
+        return address
+      else
+        return ""
+      end
+    end
+
+    def get_address(num)
+      filename = "lib/resource/KEN_ALL_ROME.CSV"
+      phash = open_csv(filename)
+      postnum = fmt_input(num)
+      if phash.length == 0
+        return ""
+      end
+      if postnum.length == 0
+        return ""
+      end
+      return find_zip(postnum, phash)
+    end
 
     def open_csv(filename)
       zipHash = Hash.new
@@ -11,7 +42,6 @@ module JpnZipcode
         #File.open("resource/KEN_ALL_ROME.CSV") do |csvfile|
         csvfile.each_line do |fileline|
           zipHash[fileline.split(",")[0].to_s] = fileline
-          puts fileline
         end
       end
       return zipHash
@@ -20,53 +50,10 @@ module JpnZipcode
     puts %Q(class=[#{e.class}] message=[#{e.message}])
   rescue IOError => e
     puts %Q(class=[#{e.class}] message=[#{e.message}])
-  end
 
-  def get_address(num)
-    filename = "http://xxx.csv"
-    phash = open_csv(filename)
-    postnum = fmt_input(num)
-    if phash.size = 0
-      return ""
-    end
-    if postnum.length = 0
-      return ""
-    end
-    return find_zip(postnum, phash)
-  end
 
-  def fmt_input(zipcode)
-    if zipcode.to_s =~ /^\d{7}$/
-      return zipcode
-    elsif zipcode.to_s =~ /^\d{3}-\d{4}$/
-      zipcode = zipcode.delete("-")
-      return zipcode
-    else
-      return ""
-    end
-  end
 
-  def find_zip(zipcode, zip_hash)
-    address = zip_hash[zipcode]
-    if address.is_a? String
-      return address
-    else
-      return ""
-    end
-  end
 
-  def get_address(num)
-    filename = "http://xxx.csv"
-    phash = open_csv(filename)
-    postnum = fmt_input(num)
-    if phash.size = 0
-      return ""
-    end
-    if postnum.length = 0
-      return ""
-    end
-    return find_zip(postnum, phash)
   end
-
 end
 
--- test/jpn_zipcode_test.rb
+++ test/jpn_zipcode_test.rb
@@ -6,36 +6,21 @@ class JpnZipcodeTest < Minitest::Test
   def setup
       @main = ::JpnZipcode::Main.new
   end
-  def test_that_it_has_a_version_number
-    refute_nil ::JpnZipcode::VERSION
-  end
-
-#  def test_it_does_something_useful
-#    assert false
-#  end
 
   def test_open_csv
-
       empty_hash = Hash.new
-      test_hash = {
-"0600000" => "0600000,北海道,札幌市　中央区,以下に掲載がない場合,HOKKAIDO,SAPPORO SHI CHUO KU,IKANIKEISAIGANAIBAAI",
-"0640941" => "0640941,北海道,札幌市　中央区,旭ケ丘,HOKKAIDO,SAPPORO SHI CHUO KU,ASAHIGAOKA",
-"0600041" => "0600041,北海道,札幌市　中央区,大通東,HOKKAIDO,SAPPORO SHI CHUO KU,ODORIHIGASHI"
-       }
+      test_hash = {"0600000" => "0600000,北海道,札幌市　中央区,以下に掲載がない場合,HOKKAIDO,SAPPORO SHI CHUO KU,IKANIKEISAIGANAIBAAI\n"}
         
       assert_equal test_hash, @main.open_csv("test/resource/test.csv"), 'OK'
 #      assert_equal empty_hash, @main.open_csv("dummy.csv") 'NG'
   end
 
- # def test_it_does_something_useful
- #  assert false
- # end
    def test_fmt_input
        assert_equal '1234567', @main.fmt_input('1234567'), 'OK'
        assert_equal '1234567', @main.fmt_input('123-4567'), 'OK'
        assert_equal "", @main.fmt_input('1'), 'NG'
        assert_equal "", @main.fmt_input("12345678"), 'NG'
-    end
+   end
 
     def test_find_zip()
       hash = {}
@@ -43,4 +28,9 @@ class JpnZipcodeTest < Minitest::Test
       assert_equal "TOKYO", @main.find_zip("1234567", hash)
       assert_equal "", @main.find_zip("0000000", hash)
     end
+
+    def test_get_address()
+      assert_equal "1400002,東京都,品川区,東品川,TOKYO TO,SHINAGAWA KU,HIGASHISHINAGAWA\n", @main.get_address("1400002")
+    end
 end
+
